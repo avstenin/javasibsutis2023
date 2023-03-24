76a82a0f124b7a83c7e6ae4f427bf4082104c14d
Temnikov Konstantin <k.s.temnikov@gmail.com>
2023-03-24 23:31:42 +0700

    init DnsServers in maon

--- lab2/temnikov/l2/src/Main.java
+++ lab2/temnikov/l2/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
-    int numbDnsServers = 0;
     public static void main(String[] args) {
-
+        int numbDnsServers = 0;
+        DnsServer[] dnsServers = new DnsServer[numbDnsServers];
     }
 }
