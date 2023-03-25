f3a1c5b69b5214f346387929eefaf0d9acca88fb
Anna Lugovaya <annaly4499@cyber.sibsutis.ru>
2023-03-25 16:26:42 +0700

    Add code from lab1

---
+++ lab2/Lugovaya/ServerInfo.java
@@ -0,0 +1,33 @@
+public class ServerInfo implements Comparable
+{
+    private Float averageTime = -1.0f;
+    private String serverAddress;
+
+    public ServerInfo(Float averageTime, String serverAddress) {
+        this.averageTime = averageTime;
+        this.serverAddress = serverAddress;
+    }
+
+    public Float getAverageTime() {
+        return averageTime;
+    }
+
+    public String getServerAddress() {
+        return serverAddress;
+    }
+
+    @Override
+    public int compareTo(Object obj)
+    {
+        ServerInfo tmp = (ServerInfo)obj;
+        if(this.averageTime < tmp.averageTime)
+        {
+            return -1;
+        }
+        else if(this.averageTime > tmp.averageTime)
+        {
+            return 1;
+        }
+        return 0;
+    }
+}
