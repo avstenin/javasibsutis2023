bcdd7a4d3cde981399520c2e2c53822a83fcf957
Temnikov Konstantin <k.s.temnikov@gmail.com>
2023-03-25 13:37:41 +0700

    implemented data reading

--- lab2/temnikov/l2/src/Main.java
+++ lab2/temnikov/l2/src/Main.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of DNS servers: ");
+        int numbDnsServers = scanner.nextInt();
+        String[] dnsServers = new String[numbDnsServers];
+        for(int i = 0; i < numbDnsServers; ++i) {
+            System.out.print("Enter the IP address of DNS server " + (i+1) + ": ");
+            dnsServers[i] = scanner.next();
+        }
     }
 }
