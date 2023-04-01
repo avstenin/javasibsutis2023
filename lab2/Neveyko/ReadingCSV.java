605cf0d786a8ce59aa8bea9cda69253d75ad78a8
NeveykoArt <neveiko29@gmail.com>
2023-04-01 20:39:38 +0700

    write readcsv func

--- lab2/Neveyko/ReadingCSV.java
+++ lab2/Neveyko/ReadingCSV.java
@@ -1,6 +1,17 @@
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
 public class ReadingCSV {
     public void read() throws IOException {
-        
+        var reader = new BufferedReader(new FileReader("dns_stat.csv"));
+        String line;
+        var lines_count = 0;
+        while ((line = reader.readLine()) != null) {
+            if (lines_count != 0) {
+                String[] dns_data = line.split(",");
+                System.out.println("address = " + dns_data[0] + ", avg time = " + dns_data[1]);
+            }
+            ++lines_count;
+        }
     }
 }
